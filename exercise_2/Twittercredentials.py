5acd3b9699f4f37c2eaf7c7ec3d6fe12508d3b7f
heshan <brianhe1992@gmail.com>
2017-11-30 09:10:35 -0800

    Ex 2 First Commit

--- exercise_2/Twittercredentials.py
+++ exercise_2/Twittercredentials.py
@@ -1,16 +1,16 @@
 import tweepy
 
-consumer_key = "YourConsumerkey";
+consumer_key = "y3FSzjpBwaCqlPd9JFws64Mdy";
 #eg: consumer_key = "YisfFjiodKtojtUvW4MSEcPm";
 
 
-consumer_secret = "YourConsumerSecret";
+consumer_secret = "yiCiGgMVaRegZBeGcl8cGkCPX8JHQYitUeuI4qBijPNtpJk9HL";
 #eg: consumer_secret = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
 
-access_token = "YourAcessToken";
+access_token = "924354335114272768-HO385GSfX1Rf90MLOHW1YAY1p4wlEz3";
 #eg: access_token = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
 
-access_token_secret = "YourTokenSecret";
+access_token_secret = "qlmdWWitsnDUdO2LSrl3Svsv0zF9UgvkHtaQ0y9lA7FwW";
 #eg: access_token_secret = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
 
 
