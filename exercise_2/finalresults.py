19616cf20b669316c67a42b01fe69f03738b9ff3
heshan <brianhe1992@gmail.com>
2017-12-01 08:41:07 -0800

    script updated

---
+++ exercise_2/finalresults.py
@@ -0,0 +1,26 @@
+import sys
+
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+if len(sys.argv) > 2 | len(sys.argv) < 1:
+    print "invalid argument"
+    exit(1)
+
+conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
+
+cur = conn.cursor()
+
+if len(sys.argv) == 2:
+    word = sys.argv[1]
+    cur.execute("SELECT count from tweetwordcount WHERE word = %s", (word, ))
+    result = cur.fetchone()
+    print "Total number of occurrences of \"", word, "\":", result[0]
+
+if len(sys.argv) == 1:
+    cur.execute("SELECT word, count from tweetwordcount)
+    records = cur.fetchall()
+    print records
+
+conn.commit()
+conn.close()
